d8fa96514f25ebcbe5a67d4036046a9a42bef3db
Jonathan Chau <jonathan.chau06ios@gmail.com>
2025-01-24 21:44:41 -0800

    Minor update to landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image';
+// import Image from "next/image";
 
 export default function Home() {
-	return (
-		<div className='flex items-center justify-center w-screen h-screen'>
-			<h1 className='text-5xl text-red-400'>Shelterfy</h1>
-		</div>
-	);
+  return (
+    <main>
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <div>
+          <h1 className="text-5xl text-red-400">Shelterfy</h1>
+          <h2 className="text-3xl text-white-400">
+            Reliable Shelters, Reliable People
+          </h2>
+        </div>
+      </div>
+    </main>
+  );
 }
