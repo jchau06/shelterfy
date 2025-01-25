6db84e2fbba130743420a4a05b12cd525dfb1676
Arvick <macalaladjustin@gmail.com>
2025-01-24 21:44:59 -0800

    created initial schema for database

--- src/db/tables.sql
+++ src/db/tables.sql
@@ -1,7 +1,23 @@
--- CREATE TABLE IF NOT EXISTS items (
---     item_id SERIAL PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     quantity INT NOT NULL DEFAULT 0,
---     unit VARCHAR(20), -- e.g., grams, liters, packets
---     expiration_date DATE
--- );
+CREATE TABLE IF NOT EXISTS login_info (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS locations(
+    loc_id SERIAL PRIMARY KEY,
+    address_line_one TEXT NOT NULL,
+    address_line_two TEXT,
+    locality TEXT,
+    state_abbr VARCHAR(5),
+    zip_code TEXT,
+    is_shelter BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS user_to_location(
+    user_id SERIAL PRIMARY KEY REFERENCES login_info(user_id),
+    loc_id SERIAL PRIMARY KEY REFERENCES locations(loc_id)
+);
