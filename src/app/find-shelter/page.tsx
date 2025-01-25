4183e06fb07aca008ab241d70c8a6bc378cbdf93
Carson Davis <cmd52465@gmail.com>
2025-01-25 10:55:48 -0800

    map integration

--- src/app/find-shelter/page.tsx
+++ src/app/find-shelter/page.tsx
@@ -1,9 +1,23 @@
+'use client';
 
-export default function Page() {
-    return (
-        <div>
-            <h1>Placeholder</h1>
-        </div>
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import React from 'react';
 
-    )
-}
+const FindShelter = () => {
+	// 34.0467° N, 118.5464° W
+	const position = { lat: 34.0467, lng: -118.5464 };
+
+	return (
+		<APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
+			<div className='w-[78vw] h-[80vh]'>
+				<Map
+					defaultCenter={position}
+					defaultZoom={9}
+					gestureHandling='greedy'
+				></Map>
+			</div>
+		</APIProvider>
+	);
+};
+
+export default FindShelter;
